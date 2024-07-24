7a1954472eecdce845d98c5e78b3719c6181d291
Irkaraz <jessica.js1306@gmail.com>
2024-07-24 17:24:42 -0300

    Querying Objects with JPA - pt.1

--- 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -22,8 +24,23 @@ public class CruddemoApplication {
 
             // createMultipleStudents(studentDAO);
 
-            readStudent(studentDAO);
+            // readStudent(studentDAO);
+
+            queryForStudents(studentDAO);
+
         };
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+
+        // get a list of students
+        List<Student> students = studentDAO.findAll();
+
+        // display list of students
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
     }
 
     private void readStudent(StudentDAO studentDAO) {
